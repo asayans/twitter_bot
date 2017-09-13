9c72af05780a7b6394b59ac7a54dabf25d716cdf
Alex (sysfiend) Sayáns <pitagoras4444@hotmail.es>
2017-09-13 03:23:06 +0200

    Added check for duplicates

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import tweepy
 import time
 
-# Auth
-auth = tweepy.OAuthHandler('consumer_token', 'consumer_secret')
-auth.set_access_token('access_token', 'access_token_secret')
+'''
+    Auth
+'''
+auth = tweepy.OAuthHandler('7YZhADlmNX35BWIIq2dpVCF9q', 'i2k1boNIaGj0Fgii7QtfWj5FlETXg2mBLFkq7q7fzKOJXqQ68R')
+auth.set_access_token('907665707202224128-2ExqKKi4ACUmN6y2palkKc8Ve1B1cjt', 'JDR0JFTRucPUzHWT1tGMELy3Yxh8KCT2jaGdepFp1izWI')
 api = tweepy.API(auth)
 
-# FIRE! #
-already_done = []
+'''
+    Functions
+'''
 
-# get last message's text and tweet it
+# Check duplicates in already tweeted messages
+# returns True if duplicated, False if not
+def is_duplicate(text):
+    with open('already_done.txt', 'r') as file:
+        done = [line.rstrip() for line in file]
+        if text in done:
+            return True
+        else:
+            return False
+
+# Get last message's text and tweet it
 def tweet_last_message():
     message = api.direct_messages(count='1', full_text='True')[0]
-    if message.id in already_done or message.id == '':
-        time.sleep(30)
-    else:
+    if not is_duplicate(message.text):
         text_to_tweet = message.text
         api.update_status(status=text_to_tweet)
-        already_done.append(message.id)
+        with open('already_done.txt', 'a') as file:
+            file.write(text_to_tweet)
 
-
+'''
+    Fire!
+'''
 while True:
     tweet_last_message()
     time.sleep(30)
