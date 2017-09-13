efa72ac3a639e71c242cd1c2e4a235b146cdfe53
Alex (sysfiend) Sayáns <pitagoras4444@hotmail.es>
2017-09-13 16:12:49 +0200

    Added insta delivery using StreamListener

--- project/main.py
+++ project/main.py
@@ -2,16 +2,29 @@ import tweepy
 import time
 
 '''
-    Auth
+    OAuth
 '''
+consumer_key = 'x'
+consumer_secret = 'x'
+access_token = 'x'
+access_token_secret = 'x'
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+'''
+    StreamListener
+'''
+class Listener(tweepy.StreamListener):
+    def on_direct_message(self):
+        tweet_last_message()
+
+        return True
 
 '''
     Functions
 '''
-
 # Check duplicates in already tweeted messages
 # returns True if duplicated, False if not
 def is_duplicate(text):
@@ -24,16 +37,28 @@ def is_duplicate(text):
 
 # Get last message's text and tweet it
 def tweet_last_message():
-    message = api.direct_messages(count='1', full_text='True')[0]
-    if not is_duplicate(message.text):
-        text_to_tweet = message.text
-        api.update_status(status=text_to_tweet)
-        with open('already_done.txt', 'a') as file:
-            file.write('\n' + text_to_tweet)
+    last_id = 0
+    while True:
+        if last_id == 0:
+            args = 'count=1'
+        else:
+            args = 'since_id=last_id'
+
+        message = api.direct_messages(args)[0]
+        last_id = message.id
+        print(message)
+
+        if not is_duplicate(message.text):
+            text_to_tweet = message.text
+            api.update_status(status=text_to_tweet)
+            with open('already_done.txt', 'a') as file:
+                file.write('\n' + text_to_tweet)
+
+        time.sleep(1)
+
 
 '''
     Fire!
 '''
-while True:
-    tweet_last_message()
-    time.sleep(30)
+action = Listener()
+action.on_direct_message()
